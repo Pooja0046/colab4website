e7dc926e214d3f5e76e5cb1743ac109c83db7e0f
Pooja0046 <22499730@stu.mmu.ac.uk>
2025-05-05 14:44:58 +0100

    Added elements
    First test

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,3 @@
-const screen = document.getElementById('screen');
-const playBtn = document.getElementById('play');
-const nextBtn = document.getElementById('next');
-const prevBtn = document.getElementById('prev');
 
-// Dummy frames (could later be videos/images/text)
-const frames = [
-    "Welcome to the Glitchy TV Experience",
-    "Documenting Reality... Please Wait...",
-    "Glitch Detected. Stay Tuned.",
-    "Truth Behind the Simulation...",
-    "End of Transmission."
-];
 
-let currentFrame = 0;
-
-function showFrame(index) {
-    screen.innerHTML = `<p>${frames[index]}</p>`;
-}
-
-// Button Handlers
-playBtn.addEventListener('click', () => {
-    showFrame(currentFrame);
-});
-
-nextBtn.addEventListener('click', () => {
-    currentFrame = (currentFrame + 1) % frames.length;
-    showFrame(currentFrame);
-});
-
-prevBtn.addEventListener('click', () => {
-    currentFrame = (currentFrame - 1 + frames.length) % frames.length;
-    showFrame(currentFrame);
-});
 
